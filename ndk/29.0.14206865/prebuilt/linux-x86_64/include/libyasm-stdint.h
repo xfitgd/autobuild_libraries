cb8481de4e76ff5955b4b029bdeae1e70001a5a4
xfitgd <xfitgd@gmail.com>
2026-01-04 20:47:44 +0900

    Remove obsolete NDK binaries and associated files from the prebuilt directory, including make, ndk-gdb, ndk-stack, ndk-which, and various YASM-related headers and libraries. This cleanup reduces clutter and ensures that only necessary files remain in the project.

--- ndk/29.0.14206865/prebuilt/linux-x86_64/include/libyasm-stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _YASM_LIBYASM_STDINT_H
-#define _YASM_LIBYASM_STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "yasm 1.3.0"
-/* generated using /mnt/disks/build-disk/src/android/ndk-r29-release/prebuilts/clang/host/linux-x86/clang-r563880c/bin/clang --target=x86_64-linux-gnu --sysroot=/mnt/disks/build-disk/src/android/ndk-r29-release/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/sysroot -L/mnt/disks/build-disk/src/android/ndk-r29-release/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/lib/gcc/x86_64-linux/4.8.3 -B/mnt/disks/build-disk/src/android/ndk-r29-release/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/lib/gcc/x86_64-linux/4.8.3 -L/mnt/disks/build-disk/src/android/ndk-r29-release/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/x86_64-linux/lib64 -B/mnt/disks/build-disk/src/android/ndk-r29-release/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/x86_64-linux/lib64 -L/mnt/disks/build-disk/src/android/ndk-r29-release/prebuilts/clang/host/linux-x86/clang-r563880c/lib64 -B/mnt/disks/build-disk/src/android/ndk-r29-release/prebuilts/clang/host/linux-x86/clang-r563880c/lib64 -Os -fomit-frame-pointer -w -fuse-ld=lld -s */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
